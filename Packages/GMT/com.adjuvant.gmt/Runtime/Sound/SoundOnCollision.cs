d739c74696ccee77b460b8fac43dfbc9c972e63b
Adjuvant <tom@layeraudio.com>
2021-02-18 18:16:33 +0000

    feat(sound): add sound tools to package
    add basic audio triggering functionality

---
+++ Packages/GMT/com.adjuvant.gmt/Runtime/Sound/SoundOnCollision.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class SoundOnCollision : MonoBehaviour
+{
+    private AudioSource sound;
+
+    // Use this for initialization
+    private void Start()
+    {
+        sound = GetComponent<AudioSource>();
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        sound.pitch = Random.Range(0.76f, 1.35f);
+        sound.PlayOneShot(sound.clip,Random.Range(0.85f,1f));
+        
+    }
+}
